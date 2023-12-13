b854635e46945630bf37aaad6cd0c5cdc7497e33
Usama Jalal <usamajalal17@gmail.com>
2023-12-14 00:17:42 +0500

    upd

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -134,7 +134,41 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        // Validate and perform login
+        try {
+            $validateUser = Validator::make($request->all(), 
+            [
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+
+            if($validateUser->fails()){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors()
+                ], 401);
+            }
+
+            if(!Auth::attempt($request->only(['email', 'password']))){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Email & Password does not match with our record.',
+                ], 401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+            return response()->json([
+                'user' => $user,
+                'token' => $user->createToken("API TOKEN")->plainTextToken,
+                'status' => true,
+            ], 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
     }
 
     public function logout(Request $request)
