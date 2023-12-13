5adc7cfd8ac1261135ea62178e0c4676d46dedc7
Usama Jalal <usamajalal17@gmail.com>
2023-12-13 20:31:58 +0500

    upd

--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-    Route::post('/logout', 'AuthController@logout');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
 // Authentication Routes
 Route::post('/register',[AuthController::class, 'register'])->name('register');
-Route::post('/login', 'AuthController@login')->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 // Sanctum routes for SPA authentication
 Route::middleware('auth:sanctum')->group(function () {
